822791cdc28dd0028ccfcfb446aba44f6e21a884
Tyom Semonov <inspiro@gmail.com>
2015-10-20 14:52:27 +0100

    Set up Mojular with Middleman

--- config.rb
+++ config.rb
@@ -16,14 +16,16 @@ page '/*.txt', layout: false
 # proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
 #  which_fake_page: "Rendering a fake page with a local variable" }
 
+set :layout, 'base'
+
 ###
 # Helpers
 ###
 
 # Reload the browser automatically whenever files change
-# configure :development do
-#   activate :livereload
-# end
+configure :development do
+  activate :livereload
+end
 
 # Methods defined in the helpers block are available in templates
 # helpers do
@@ -31,6 +33,17 @@ page '/*.txt', layout: false
 #     "Helping"
 #   end
 # end
+
+# Load Sass paths and copy images & layouts
+require 'find'
+`mkdir -p "#{config.source}/#{config.images_dir}" "#{config.source}/#{config.layouts_dir}"`
+Find.find('node_modules').grep(/mojular[a-z-]+\/package\.json/).map do |package|
+  sassPaths = JSON.parse(IO.read(package))['sassPaths']
+  dirname = File.dirname(package)
+  sassPaths.map { |path| Sass.load_paths << File.expand_path(path, File.directory?(path) ? '' : dirname) } if sassPaths
+  FileUtils.cp_r Find.find(dirname).grep(/images\//), "#{config.source}/#{config.images_dir}"
+  FileUtils.cp_r Find.find(dirname).grep(/layouts\/erb\//), "#{config.source}/#{config.layouts_dir}"
+end
 
 # Build-specific configuration
 configure :build do
